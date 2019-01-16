26fc5837c2f184176b51579b0fce5fb189e19ee7
prabeensoti <prabeen.soti@hotmail.com>
2019-01-16 23:16:36 +0545

    achivements

--- src/app/home/achivement/achivement.component.ts
+++ src/app/home/achivement/achivement.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./achivement.component.css']
 })
 export class AchivementComponent implements OnInit {
+  achivements: string [] = [];
+  achivement: string[] = null;
+  indexs = 0;
+    nextImage() {
+      if (this.indexs < this.achivements.length - 1 ) {
+        this.indexs++;
+      } else {
+        this.indexs = 0;
+      }
+     // this.photoscr = this.achivements[this.indexs];
+    }
+    previousImage() {
+      if (this.indexs === 0) {
+        this.indexs = this.achivements.length - 1;
+      } else {
+        this.indexs--;
+      }
+     // this.photoscr = this.achivements[this.indexs];
+    }
+    changeImage(i: number) {
+      this.indexs = i;
+      //this.photoscr = this.achivements[i];
+    }
+    autoChange() {
+      setInterval(() => {
+        this.nextImage();
+       }, 5000);
+    }
 
   constructor() { }
 
   ngOnInit() {
+    //this.photoscr = this.achivements[0];
+    this.autoChange();
   }
 
 }
